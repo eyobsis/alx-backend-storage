a5ea7aebeb00118a999d481f22e3ff292f0ad9c4
eyobsis <ehtube2020@gmail.com>
2024-03-27 23:21:31 +0000

    redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Caching request module
+"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+
+def track_get_page(fn: Callable) -> Callable:
+    """Decorator function to track and cache HTTP requests."""
+    @wraps(fn)
+    def wrapper(url: str) -> str:
+        """
+        Wrapper function that:
+        - checks whether a URL's data is cached
+        - tracks the number of times get_page is called
+        """
+        client = redis.Redis()
+        client.incr(f'count:{url}')
+        cached_page = client.get(f'{url}')
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(f'{url}', response, 10)
+        return response
+    return wrapper
+
+
+@track_get_page
+def get_page(url: str) -> str:
+    """Makes an HTTP request to a given endpoint."""
+    response = requests.get(url)
+    return response.text
+
